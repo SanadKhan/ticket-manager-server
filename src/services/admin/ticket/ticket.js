071817e2b1d3df2623a5eece8040523566658676
SanadKhan <khansanad72485@gmail.com>
2023-05-20 18:21:27 +0530

    added owner and assigned to name

--- src/services/admin/ticket/ticket.js
+++ src/services/admin/ticket/ticket.js
@@ -1,59 +1,65 @@
 import { Ticket } from '../../../models';
 
-export const create = async(values) => {
+export const create = async (values) => {
     try {
         const ticket = new Ticket(values);
         await ticket.save();
         // emailService.sendWelcomeEmail(ticket.email, ticket.name, values.password);
-        return { status: 201, msgText: 'Created Successfully! ',
-        success: true, ticket }
+        return {
+            status: 201, msgText: 'Created Successfully! ',
+            success: true, ticket
+        }
     } catch (error) {
         throw error;
     }
 };
 
-export const readAll = async({page, perPage, whereClause={}}) => {
+export const readAll = async ({ page, perPage, whereClause = {} }) => {
     try {
         const ticket = await Ticket.find(whereClause)
-        .sort({ _id: -1 }).skip(((perPage * page) - perPage))
-        .limit(perPage);
-        if(!ticket.length > 0) {
-            return { status: 404 , msgText: "Ticket does not exists!" ,success: false }
+            .populate([
+                { 'path': 'owner', 'select': ['name'] },
+                { 'path': 'assigned_to', 'select': ['name'] },
+            ])
+            .sort({ _id: -1 }).skip(((perPage * page) - perPage))
+            .limit(perPage);
+        if (!ticket.length > 0) {
+            return { status: 404, msgText: "Ticket does not exists!", success: false }
         }
-        return { status: 200, success: true, ticket}
+        return { status: 200, success: true, ticket }
     } catch (error) {
         throw error;
     }
 };
 
-export const read = async(id) => {
+export const read = async (id) => {
     try {
         const ticket = await Ticket.findById(id)
-        if(!ticket) {
-            return { status: 404 , msgText: "Ticket does not exists!" ,success: false }
+        if (!ticket) {
+            return { status: 404, msgText: "Ticket does not exists!", success: false }
         }
-        return { status: 200, success: true, ticket}
+        return { status: 200, success: true, ticket }
     } catch (error) {
         throw error;
     }
 };
 
-export const update = async(id, values) => {
+export const update = async (id, values) => {
     try {
         const ticket = await Ticket.findByIdAndUpdate(id, values);
-        if(!ticket) {
-            return { status: 404 , msgText: "Ticket does not exists!" ,success: false }
-        }  
-        return { status: 200, msgText: 'Updated Successfully! ',success: true}
+        if (!ticket) {
+            return { status: 404, msgText: "Ticket does not exists!", success: false }
+        }
+        return { status: 200, msgText: 'Updated Successfully! ', success: true }
     } catch (error) {
         throw error;
     }
 };
 
-export const remove = async(ids)=> {
+export const remove = async (ids) => {
     try {
-        await Ticket.deleteMany({"_id": { "$in" : ids}});
-        return { status: 200, msgText: 'Deleted Successfully!', success: true}
+        await Ticket.deleteMany({ "_id": { "$in": ids } });
+        return { status: 200, msgText: 'Deleted Successfully!', success: true }
     } catch (error) {
         throw error;
     }
